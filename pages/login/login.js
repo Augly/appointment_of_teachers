bcd209d6fbc54a94911df7a6cabf9cc55d98c523
zero <1137774080@qq.com>
2019-04-09 23:52:04 +0800

    开始对接接口

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,37 +6,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-    user_name: '',
-    user_password: ''
+    user_phone: '',
+    user_password: '',
+    code_type: '验证码'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
+  //获取用户信息
+  getUserInfo(res) {
+    console.log(res)
+    this.to_index()
+  },
+  //发送验证码
+  send_code() {
+    if (!/^1[34578]\d{9}$/.test(this.data.user_phone)) {
+      config.mytoast('请输入正确的手机号!')
+      return false
+    }
+    if (this.data.code_type == '验证码') {
+      config.ajax('POST', {
+        phone: this.data.user_phone
+      }, '/login/send_message', res => {
+        this.setData({
+          code_type: 60,
+        })
+        let st = setInterval(() => {
+          let n = this.data.code_type--
+            if (n == 1) {
+              this.setData({
+                code_type: '验证码'
+              })
+              clearInterval(st)
+            } else {
+              n--
+              this.setData({
+                code_type: n
+              })
+            }
+        }, 1000)
+      })
 
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
   //获取用户名和密码
   get_user(e) {
-    console.log(e)
     switch (e.currentTarget.dataset.type) {
-      case "user_name":
+      case "user_phone":
         this.setData({
-          user_name: e.detail.value
+          user_phone: e.detail.value
         })
         break;
       case 'user_password':
@@ -48,50 +83,69 @@ Page({
 
     }
   },
+  //进行登陆操作
   to_index() {
-    if (this.data.user_name != '' && this.data.user_password != '') {
-      wx.switchTab({
-        url: '/pages/ordel/ordel',
-        success: function (res) { },
-        fail: function (res) { },
-        complete: function (res) { },
-      })
-    } else {
-      config.mytoast('请输入用户名和秘密')
+    if (!/^1[34578]\d{9}$/.test(this.data.user_phone)) {
+      config.mytoast('请输入正确的手机号!')
+      return false
     }
+    if (this.data.user_password == '') {
+      config.mytoast('请输入短信验证码!')
+      return false
+    }
+    config.ajax('POST', {
+      phone: this.data.user_phone,
+      code: this.data.user_password
+    }, '/login/user_login', res => {
+      wx.setStorage({
+        key: 'user_token',
+        data: res.data.data.user_token,
+        success: function(res) {
+          wx.switchTab({
+            url: '/pages/ordel/ordel',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+          })
+        },
+        fail: function(res) {},
+        complete: function(res) {}
+      })
+
+    })
   },
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,11 +1,9 @@
 //主域名
-const myapp = getApp()
-const https = "https://lingyiil.dazhu-ltd.cn/index.php/api/"
-
+const https = "http://yueke.dazhu-ltd.cn/user"
 /**
  * 时间转换
  */
-function timeFormatNotime(time) {
+function timeForm(time) {
   var date = new Date(time * 1000),
     curDate = new Date(),
     year = date.getFullYear(),
@@ -15,20 +13,27 @@ function timeFormatNotime(time) {
     minute = date.getMinutes(),
     curYear = curDate.getFullYear(),
     curHour = curDate.getHours(),
-    timeStr;
+    timeStr, btTimeStr;
+
   if (minute < 10) {
+    remW
     minute = '0' + minute
   }
+  if (month < 10) {
+    month = '0' + month
+  }
+  if (day < 10) {
+    day = '0' + day
+  }
+  btTimeStr = year + '-' + month + '-' + day
   if (year < curYear) {
-    // timeStr = year + '年' + month + '月' + day + '日' + hour + ':' + minute;
-    timeStr = year + '/' + month + '/' + day;
+    timeStr = year + '-' + month + '-' + day;
+
   } else {
     var pastTime = curDate - date,
       pastH = pastTime / 3600000;
-
     if (pastH > curHour) {
-      // timeStr = month + '月' + day + '日 ' + '' + hour + '时' + minute + '分';
-      timeStr = year + '/' + month + '/' + day;
+      timeStr = year + '-' + month + '-' + day;
     } else if (pastH >= 1) {
       timeStr = '今天 ' + hour + ':' + minute + '分';
     } else {
@@ -40,121 +45,10 @@ function timeFormatNotime(time) {
       }
     }
   }
-  return timeStr;
-}
-function timeForm(time) {
-  var date = new Date(time * 1000),
-    curDate = new Date(),
-    year = date.getFullYear(),
-    month = date.getMonth() + 1,
-    day = date.getDate(),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    curYear = curDate.getFullYear(),
-    curHour = curDate.getHours(),
-    timeStr;
-  if (minute < 10) {
-    minute = '0' + minute
-  }
-  if (year < curYear) {
-    // timeStr = year + '年' + month + '月' + day + '日' + hour + ':' + minute;
-    timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-  } else {
-    var pastTime = curDate - date,
-      pastH = pastTime / 3600000;
-
-    if (pastH > curHour) {
-      // timeStr = month + '月' + day + '日 ' + '' + hour + '时' + minute + '分';
-      timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-    } else if (pastH >= 1) {
-      timeStr = hour + ':' + minute + '分';
-    } else {
-      var pastM = curDate.getMinutes() - minute;
-      if (pastM > 1) {
-        timeStr = pastM + '分钟前';
-      } else {
-        timeStr = '刚刚';
-      }
-    }
-  }
-  return timeStr;
-}
-function timeall(time) {
-  var date = new Date(time * 1000),
-    curDate = new Date(),
-    year = date.getFullYear(),
-    month = date.getMonth() + 1,
-    day = date.getDate(),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    curYear = curDate.getFullYear(),
-    curHour = curDate.getHours(),
-    timeStr;
-  // if (minute < 10) {
-  //   minute = '0' + minute
-  // }
-  timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-  // if (year < curYear) {
-  //   // timeStr = year + '年' + month + '月' + day + '日' + hour + ':' + minute;
-  //   timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-  // } else {
-  //   var pastTime = curDate - date,
-  //     pastH = pastTime / 3600000;
-
-  //   if (pastH > curHour) {
-  //     // timeStr = month + '月' + day + '日 ' + '' + hour + '时' + minute + '分';
-  //     timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-  //   } else if (pastH >= 1) {
-  //     timeStr = hour + ':' + minute + '分';
-  //   } else {
-  //     var pastM = curDate.getMinutes() - minute;
-  //     if (pastM > 1) {
-  //       timeStr = pastM + '分钟前';
-  //     } else {
-  //       timeStr = '刚刚';
-  //     }
-  //   }
-  // }
-  return timeStr;
-}
-function timelist(time) {
-  var date = new Date(time * 1000),
-    curDate = new Date(),
-    year = date.getFullYear(),
-    month = date.getMonth() + 1,
-    day = date.getDate(),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    curYear = curDate.getFullYear(),
-    curHour = curDate.getHours(),
-    timeStr;
-
-  if (minute < 10) {
-    minute = '0' + minute
-  }
-  timeStr = hour + ':' + minute;
-  // if (year < curYear) {
-  //   // timeStr = year + '年' + month + '月' + day + '日' + hour + ':' + minute;
-  //   timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;
-  // } else {
-  //   var pastTime = curDate - date,
-  //     pastH = pastTime / 3600000;
-
-  //   if (pastH > curHour) {
-  //     // timeStr = month + '月' + day + '日 ' + '' + hour + '时' + minute + '分';
-  //     timeStr = year + '-' + month + '-' + day + ' ' + hour + ':' + minute;;
-  //   } else if (pastH >= 1) {
-  //     timeStr = hour + ':' + minute + '分';
-  //   } else {
-  //     var pastM = curDate.getMinutes() - minute;
-  //     if (pastM > 1) {
-  //       timeStr = pastM + '分钟前';
-  //     } else {
-  //       timeStr = '刚刚';
-  //     }
-  //   }
-  // }
-  return timeStr;
+  return {
+    chatTime: timeStr,
+    btTime: btTimeStr
+  };
 }
 /**
  * 封装自定义优美的toast
@@ -219,6 +113,21 @@ function rem(height, successData) {
     success: (res) => {
       if (height != null && height != undefined && height != '') {
         var myheight = res.windowHeight - res.windowWidth / 750 * height
+      } else {
+        var myheight = res.windowHeight
+      }
+      successData(myheight)
+    },
+    fail: function (res) { },
+    complete: function (res) { },
+  })
+}
+
+function remW(height, successData) {
+  wx.getSystemInfo({
+    success: (res) => {
+      if (height != null && height != undefined && height != '') {
+        var myheight = res.windowWidth / 750 * height
       } else {
         var myheight = res.windowHeight
       }
@@ -325,81 +234,12 @@ function getData(e, name) {
   return e.currentTarget.dataset[name]
 }
 /**
- * 自定义封装支付函数
- */
-function pay(res, successData, errorData) {
-  console.log(res.data.data)
-  wx.requestPayment({
-    "timeStamp": res.data.data.timeStamp,
-    "nonceStr": res.data.data.nonceStr,
-    "package": res.data.data.package,
-    "signType": "MD5",
-    "paySign": res.data.data.paySign,
-    "success": function (res) {
-      wx.showToast({
-        title: '支付完成',
-        icon: "success",
-        duration: 1500,
-        success: function (data) {
-          successData(data)
-        }
-      })
-    },
-    "fail": function (res) {
-      if (errorData) {
-        errorData(res)
-      }
-      wx.showToast({
-        title: '取消支付成功！',
-        icon: "success",
-        duration: 1500,
-      })
-    }
-  })
-}
-/**
- * 自定义获取token函数
- */
-function gitToken(successData) {
-  wx.login({
-    success: (res) => {
-      wx.request({
-        url: https + token,
-        method: 'POST',
-        header: {
-          'content-type': "application/x-www-form-urlencoded",
-          'S-type': 'm',
-          'S-token': wx.getStorageSync('token')
-        },
-        data: {
-          code: res.code
-        },
-        success: (res) => {
-          wx.setStorageSync('token', res.data.data.token)
-          if (successData) {
-            successData(res)
-          }
-        },
-        error(res) {
-
-        },
-        complete(res) {
-
-        }
-      })
-    },
-    fail: function (res) { },
-    complete: function (res) { },
-  })
-}
-/**
  * 自定义request请求基类
  */
 function ajax(Type, params, url, successData, errorData, completeData, imgurl) {
   var methonType = "application/json";
-  // methonType = "application/x-www-form-urlencoded"
   //访问的主域名
-  var https = "https://lingyiil.dazhu-ltd.cn/index.php/api/"
+  var https = "http://yueke.dazhu-ltd.cn/user"
   if (Type === 'PUT') {
     methonType = "application/x-www-form-urlencoded"
   }
@@ -409,67 +249,125 @@ function ajax(Type, params, url, successData, errorData, completeData, imgurl) {
   if (Type == "POST") {
     methonType = "application/x-www-form-urlencoded"
   }
-  wx.showLoading({
-    title: '数据加载中',
-    mask: true,
-    success: function (res) { },
-    fail: function (res) { },
-    complete: function (res) { },
-  })
-  if (Type != 'img') {
-    wx.request({
-      url: https + url,
-      method: Type,
-      header: {
-        'content-type': methonType,
-        // 'S-type': 'm',
-        // 'S-token': wx.getStorageSync('token')
-      },
-      data: params,
+  if (url != '/login/user_login' && url != '/login/send_message') {
+    wx.getStorage({
+      key: 'user_token',
       success: (res) => {
-        wx.hideLoading()
-        if (res.data.code == 1) {
-          successData(res)
+        console.log(res)
+        wx.showLoading({
+          title: '数据加载中',
+          mask: true,
+          success: function (res) { },
+          fail: function (res) { },
+          complete: function (res) { },
+        })
+        if (Type != 'img') {
+          wx.request({
+            url: https + url,
+            method: Type,
+            header: {
+              'content-type': methonType,
+            },
+            data: params,
+            success: (res) => {
+              wx.hideLoading()
+              if (res.data.code == 1) {
+                successData(res)
+              } else {
+                mytoast(res.data.msg)
+                if (res.data.code == -1) {
+                  wx.clearStorage('user_token')
+                  wx.redirectTo({
+                    url: '/pages/doctor/login/login',
+                    success: function (res) { },
+                    fail: function (res) { },
+                    complete: function (res) { },
+                  })
+                }
+              }
+            },
+            error(res) {
+              if (errorData) {
+                errorData(res)
+              }
+            },
+            complete(res) {
+              if (completeData) {
+                completeData(res)
+              }
+            }
+          })
         } else {
-          mytoast(res.data.msg)
-          if (res.data.code == -1) {
-            wx.clearStorage('user_token')
-            wx.redirectTo({
-              url: '/pages/login/index',
-              success: function (res) { },
-              fail: function (res) { },
-              complete: function (res) { },
+          if (imgurl) {
+            wx.uploadFile({
+              url: https + url,
+              filePath: imgurl,
+              name: 'image',
+              formData: params,
+              success: (res) => {
+                wx.hideLoading()
+                var data = JSON.parse(res.data)
+                if (data.code == 1) {
+                  successData(data)
+                } else {
+                  mytoast(data.msg)
+                }
+              },
+              error(res) {
+                if (errorData) {
+                  errorData(res)
+                }
+              },
+              complete(res) {
+                if (completeData) {
+                  completeData(res)
+                }
+              }
             })
           }
 
         }
       },
-      error(res) {
-        if (errorData) {
-          errorData(res)
-        }
-      },
-      complete(res) {
-        if (completeData) {
-          completeData(res)
-        }
+      fail: (res) => {
+        wx.redirectTo({
+          url: '/pages/doctor/login/login',
+          success: function (res) { },
+          fail: function (res) { },
+          complete: function (res) { },
+        })
       }
     })
-  } else {
-    if (imgurl) {
-      wx.uploadFile({
+  }else{
+    wx.showLoading({
+      title: '数据加载中',
+      mask: true,
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+    if (Type != 'img') {
+      wx.request({
         url: https + url,
-        filePath: imgurl,
-        name: 'file',
-        formData: params,
+        method: Type,
+        header: {
+          'content-type': methonType,
+        },
+        data: params,
         success: (res) => {
           wx.hideLoading()
-          var data = JSON.parse(res.data)
-          console.log(data)
-          if (data.code == 1) {
-            successData(data)
+          if (res.data.code == 1) {
+            successData(res)
           } else {
-            mytoast(data.msg)
+            mytoast(res.data.msg)
+            if (res.data.code == -1) {
+              wx.clearStorage('user_token')
+              wx.redirectTo({
+                url: '/pages/doctor/login/login',
+                success: function (res) { },
+                fail: function (res) { },
+                complete: function (res) { },
+              })
+            }
           }
         },
         error(res) {
@@ -483,16 +381,39 @@ function ajax(Type, params, url, successData, errorData, completeData, imgurl) {
           }
         }
       })
-    }
+    } else {
+      if (imgurl) {
+        wx.uploadFile({
+          url: https + url,
+          filePath: imgurl,
+          name: 'image',
+          formData: params,
+          success: (res) => {
+            wx.hideLoading()
+            var data = JSON.parse(res.data)
+            if (data.code == 1) {
+              successData(data)
+            } else {
+              mytoast(data.msg)
+            }
+          },
+          error(res) {
+            if (errorData) {
+              errorData(res)
+            }
+          },
+          complete(res) {
+            if (completeData) {
+              completeData(res)
+            }
+          }
+        })
+      }
 
+    }
   }
 
 
-
-
-
-
-  // console.log(app)
 
 };
 
@@ -501,11 +422,9 @@ module.exports = {
   https: https,
   ajax: ajax,
   rem: rem,
+  remW: remW,
   chooseImage: chooseImage,
   getData: getData,
   mytoast: mytoast,
-  timeFormatNotime: timeFormatNotime,
   timeForm: timeForm,
-  timelist: timelist,
-  timeall: timeall
 }
